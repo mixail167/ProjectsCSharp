2add41117e2c009366c8ab56a3cc0ccf3e81035d
mixail167 <mixailkovalev167@mail.ru>
2018-06-06 19:00:07 +0200

    Add Config

---
+++ WinForms and Console/Config/Config/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace Config
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExeConfigurationFileMap fileMap = new ExeConfigurationFileMap();
+            fileMap.ExeConfigFilename = AppDomain.CurrentDomain.BaseDirectory + @"App.config";
+            Configuration config = ConfigurationManager.OpenMappedExeConfiguration(fileMap, ConfigurationUserLevel.None);
+            ClientSettingsSection sec = (ClientSettingsSection)config.GetSection("userSettings/Section1");
+            Console.WriteLine(sec.Settings.Get("Name").Value.ValueXml.InnerText);
+            Console.WriteLine(sec.Settings.Get("Age").Value.ValueXml.InnerText);
+            sec = (ClientSettingsSection)config.GetSection("userSettings/Section2");
+            Console.WriteLine(sec.Settings.Get("Name").Value.ValueXml.InnerText);
+            Console.WriteLine(sec.Settings.Get("Age").Value.ValueXml.InnerText);
+            Console.ReadKey();
+        }
+    }
+}
