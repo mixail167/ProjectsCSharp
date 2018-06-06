8bdfc3559974415e86648aeae2b5160b0e4f91d4
mixail167 <mixailkovalev167@mail.ru>
2018-06-06 18:56:21 +0200

    Add BootUSB

---
+++ WinForms and Console/BootUSB/BootUSB/Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace BootUSB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button5 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Window;
+            this.button1.Location = new System.Drawing.Point(217, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 24);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Список дисков";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Window;
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Location = new System.Drawing.Point(350, 8);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 24);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Начать операцию";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.Window;
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(350, 38);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(129, 24);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Отменить операцию";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(128, 3);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            9,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(81, 20);
+            this.numericUpDown1.TabIndex = 6;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.Window;
+            this.button5.Location = new System.Drawing.Point(350, 68);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(112, 24);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Очистить консоль";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Обычное",
+            "Быстрое"});
+            this.comboBox1.Location = new System.Drawing.Point(130, 61);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(79, 21);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 56);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(5);
+            this.label2.Size = new System.Drawing.Size(110, 23);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Форматирование:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::BootUSB.Properties.Resources.flash;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(485, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(127, 127);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "NTFS",
+            "FAT32"});
+            this.comboBox2.Location = new System.Drawing.Point(130, 92);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(5);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(79, 21);
+            this.comboBox2.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 87);
+            this.label3.Name = "label3";
+            this.label3.Padding = new System.Windows.Forms.Padding(5);
+            this.label3.Size = new System.Drawing.Size(119, 23);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Файловая система:";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.comboBox2, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.numericUpDown1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.comboBox1, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 5);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(332, 118);
+            this.tableLayoutPanel1.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 30);
+            this.label5.Name = "label5";
+            this.label5.Padding = new System.Windows.Forms.Padding(5);
+            this.label5.Size = new System.Drawing.Size(83, 23);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Буква диска:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 0);
+            this.label4.Name = "label4";
+            this.label4.Padding = new System.Windows.Forms.Padding(5);
+            this.label4.Size = new System.Drawing.Size(58, 23);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Диск №";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(128, 33);
+            this.textBox1.MaxLength = 1;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(81, 20);
+            this.textBox1.TabIndex = 16;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "bootsect";
+            this.openFileDialog1.Filter = "Boot Sector Registration Tool|bootsect.exe";
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.Description = "Выберите каталог файлов, которые необходимо скопировать";
+            this.folderBrowserDialog1.ShowNewFolderButton = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.Black;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.White;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 136);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(601, 294);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(624, 442);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "BootUSB";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ WinForms and Console/BootUSB/BootUSB/Form1.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BootUSB
+{
+    public partial class Form1 : Form
+    {
+        ProcessStartInfo psi;
+        Process process;
+        string text;
+        int stage = 0;
+        string letter;
+
+        /// <summary>
+        /// Конструктор формы
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+            psi = new ProcessStartInfo();
+            psi.Verb = "runas";
+            psi.CreateNoWindow = true;
+            psi.RedirectStandardOutput = true;
+            psi.RedirectStandardInput = true;
+            psi.StandardOutputEncoding = Encoding.GetEncoding(866);
+            psi.UseShellExecute = false;
+            text = string.Empty;
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
+        }
+
+        /// <summary>
+        /// Немедленная остановка процесса
+        /// </summary>
+        /// <returns>Значение, определяющее, следовало ли останавливать процесс</returns>
+        bool KillProcess()
+        {
+            try
+            {
+                if (!process.HasExited)
+                {
+                    process.Kill();
+                    return true;
+                }
+                return false;
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                Reset(true);
+            }
+        }
+
+        /// <summary>
+        /// Сброс параметров
+        /// </summary>
+        void Reset(bool flag)
+        {
+            if (tableLayoutPanel1.InvokeRequired)
+            {
+                tableLayoutPanel1.BeginInvoke(new Action<bool>(Reset), flag);
+            }
+            else
+            {
+                if (flag)
+                {
+                    stage = 0;
+                }
+                tableLayoutPanel1.Enabled = flag;
+                button2.Enabled = flag;
+                button3.Enabled = !flag;
+            }
+        }
+
+        /// <summary>
+        /// Инитиализация и запуск процесса
+        /// </summary>
+        /// <param name="fileName">Имя процесса</param>
+        /// <param name="arguments">Аргументы процесса</param>
+        /// <returns>Значение, определяющее, был ли процесс успешно инициализирован</returns>
+        bool InitProcess(string fileName, string arguments)
+        {
+            try
+            {
+                psi.FileName = fileName;
+                psi.Arguments = arguments;
+                process = new Process();
+                process.EnableRaisingEvents = true;
+                process.OutputDataReceived += new DataReceivedEventHandler(process1_OutputDataReceived);
+                process.Exited += new EventHandler(process1_Exited);
+                process.StartInfo = psi;
+                process.Start();
+                process.BeginOutputReadLine();
+                Reset(false);
+                return true;
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+                Reset(true);
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Обновление консоли ошибок
+        /// </summary>
+        /// <param name="data">Текст для вывода</param>
+        void RefreshMainConsole(string data)
+        {
+            if (!String.IsNullOrEmpty(data) && !text.Equals(data))
+            {
+                if (richTextBox1.InvokeRequired)
+                {
+                    richTextBox1.BeginInvoke(new Action<string>(RefreshMainConsole), new[] { data });
+                }
+                else
+                {
+                    if (data.Trim().StartsWith("Завершено") && text.Trim().StartsWith("Завершено"))
+                    {
+                        try
+                        {
+                            richTextBox1.Text = richTextBox1.Text.Remove(richTextBox1.GetFirstCharIndexFromLine(richTextBox1.Lines.Length - 2), richTextBox1.Lines[richTextBox1.Lines.Length - 2].Length + 1);
+                        }
+                        catch
+                        {
+
+                        }
+                    }
+                    text = data;
+                    richTextBox1.Text += text + "\n";
+                    richTextBox1.SelectionStart = richTextBox1.TextLength;
+                    richTextBox1.ScrollToCaret();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Проверка наличия диска по его букве, введенной пользователем
+        /// </summary>
+        /// <returns>Результат проверки</returns>
+        bool CheckDrive()
+        {
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo item in drives)
+            {
+                if (item.Name[0] == letter[0])
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (InitProcess("diskpart.exe", ""))
+            {
+                try
+                {
+                    process.StandardInput.WriteLine("list disk");
+                    process.StandardInput.WriteLine("exit");
+                }
+                catch (Exception exception)
+                {
+                    KillProcess();
+                    MessageBox.Show(exception.Message);
+                }
+            }
+        }
+
+        private void process1_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            RefreshMainConsole(e.Data);
+        }
+
+        private void process1_Exited(object sender, EventArgs e)
+        {
+            switch (stage)
+            {
+                case 1:
+                    if (this.InvokeRequired)
+                    {
+                        this.Invoke(new EventHandler(process1_Exited), new[] { sender, e });
+                    }
+                    else
+                    {
+                        if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                        {
+                            stage = 2;
+                            RefreshMainConsole("Этап 2 из 3: Обновление загрузочного кода.");
+                            InitProcess(openFileDialog1.FileName, string.Format("/nt60 {0}:", letter));
+                        }
+                        else
+                        {
+                            Reset(true);
+                        }
+                    }
+                    break;
+                case 2:
+                    if (this.InvokeRequired)
+                    {
+                        this.Invoke(new EventHandler(process1_Exited), new[] { sender, e });
+                    }
+                    else
+                    {
+                        if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+                        {
+                            if (!folderBrowserDialog1.SelectedPath.EndsWith("\\"))
+                            {
+                                folderBrowserDialog1.SelectedPath += "\\";
+                            }
+                            stage = 3;
+                            RefreshMainConsole("Этап 3 из 3: Копирование файлов.");
+                            InitProcess("cmd.exe", string.Format("/c xcopy \"{0}*.*\" {1}:\\ /E /H", folderBrowserDialog1.SelectedPath, letter));
+                        }
+                        else
+                        {
+                            Reset(true);
+                        }
+                    }
+                    break;
+                case 3:
+                case 0:
+                    Reset(true);
+                    break;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (KillProcess())
+            {
+                RefreshMainConsole("Действия отменены пользователем.");
+            }
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            letter = textBox1.Text.ToUpper();
+            if (letter == string.Empty)
+            {
+                MessageBox.Show("Введите букву диска!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (CheckDrive() && MessageBox.Show("Диск с такой буквой уже существует. Продолжить?", "Внимание", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation) == DialogResult.No)
+            {
+                return;
+            }
+            if (InitProcess("diskpart.exe", ""))
+            {
+                try
+                {
+                    stage = 1;
+                    RefreshMainConsole("Этап 1 из 3: Форматирование диска.");
+                    process.StandardInput.WriteLine("select disk {0}", numericUpDown1.Value);
+                    process.StandardInput.WriteLine("clean");
+                    process.StandardInput.WriteLine("create partition primary");
+                    process.StandardInput.WriteLine("active");
+                    if (comboBox1.SelectedIndex == 0)
+                        process.StandardInput.WriteLine("format fs={0}", comboBox2.Items[comboBox2.SelectedIndex].ToString());
+                    else process.StandardInput.WriteLine("format fs={0} quick", comboBox2.Items[comboBox2.SelectedIndex].ToString());
+                    process.StandardInput.WriteLine("assign letter={0}", letter);
+                    process.StandardInput.WriteLine("exit");
+                }
+                catch (Exception exception)
+                {
+                    KillProcess();
+                    MessageBox.Show(exception.Message);
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (process != null)
+            {
+                try
+                {
+                    process.Kill();
+                }
+                catch
+                {
+
+                }
+                finally
+                {
+                    process.Close();
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = string.Empty;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text != string.Empty && (textBox1.Text.ToUpper()[0] < 65 || textBox1.Text.ToUpper()[0] > 90))
+            {
+                textBox1.Text = string.Empty;
+            }
+        }
+    }
+}
