dd5794b581415998c80452fb86fc85a22ad802fb
mixail167 <mixailkovalev167@mail.ru>
2018-06-11 13:54:41 +0200

    Add Localization

--- WPF/MineSweeper/MineSweeper/App.xaml.cs
+++ WPF/MineSweeper/MineSweeper/App.xaml.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Windows;
+using System.Windows.Navigation;
 
 namespace MineSweeper
 {
@@ -12,5 +13,76 @@ namespace MineSweeper
     /// </summary>
     public partial class App : Application
     {
+        private static List<CultureInfo> languages = new List<CultureInfo>();
+        public static event EventHandler LanguageChanged;
+
+        public App()
+        {
+            languages.Clear();
+            languages.Add(new CultureInfo("ru-RU"));
+            languages.Add(new CultureInfo("en"));
+            App.LanguageChanged += App_LanguageChanged;
+        }   
+
+        public static List<CultureInfo> Languages
+        {
+            get
+            {
+                return languages;
+            }
+        }             
+
+        public static CultureInfo Language
+        {
+            get
+            {
+                return Thread.CurrentThread.CurrentUICulture;
+            }
+            set
+            {
+                if (value == null) throw new ArgumentNullException("value");
+                if (value == Thread.CurrentThread.CurrentUICulture) return;
+                //1. Меняем язык приложения:
+                Thread.CurrentThread.CurrentUICulture = value;
+                //2. Создаём ResourceDictionary для новой культуры
+                ResourceDictionary dict = new ResourceDictionary();
+                switch (value.Name)
+                {
+                    case "ru-RU":
+                        dict.Source = new Uri(String.Format("Resources/lang.{0}.xaml", value.Name), UriKind.Relative);
+                        break;
+                    default:
+                        dict.Source = new Uri("Resources/lang.xaml", UriKind.Relative);
+                        break;
+                }
+                //3. Находим старую ResourceDictionary и удаляем его и добавляем новую ResourceDictionary
+                ResourceDictionary oldDict = (from d in Application.Current.Resources.MergedDictionaries
+                                              where d.Source != null && d.Source.OriginalString.StartsWith("Resources/lang.")
+                                              select d).First();
+                if (oldDict != null)
+                {
+                    int ind = Application.Current.Resources.MergedDictionaries.IndexOf(oldDict);
+                    Application.Current.Resources.MergedDictionaries.Remove(oldDict);
+                    Application.Current.Resources.MergedDictionaries.Insert(ind, dict);
+                }
+                else
+                {
+                    Application.Current.Resources.MergedDictionaries.Add(dict);
+                }
+                //4. Вызываем евент для оповещения всех окон.
+                LanguageChanged(Application.Current, new EventArgs());
+            }
+        }
+
+        private void Application_LoadCompleted(object sender, NavigationEventArgs e)
+        {
+            Language = MineSweeper.Properties.Settings.Default.DefaultLanguage;
+        }
+
+        private void App_LanguageChanged(Object sender, EventArgs e)
+        {
+            MineSweeper.Properties.Settings.Default.DefaultLanguage = Language;
+            MineSweeper.Properties.Settings.Default.Save();
+        }
     }
 }
--- WPF/MineSweeper/MineSweeper/Windows/MainWindow.xaml.cs
+++ WPF/MineSweeper/MineSweeper/Windows/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -22,6 +24,45 @@ namespace MineSweeper
         public MainWindow()
         {
             InitializeComponent();
+
+            App.LanguageChanged += LanguageChanged;
+            CultureInfo currLang = App.Language;
+            //Заполняем меню смены языка:
+            //menuLanguage.Items.Clear();
+            //foreach (var lang in App.Languages)
+            //{
+            //    MenuItem menuLang = new MenuItem();
+            //    menuLang.Header = lang.DisplayName;
+            //    menuLang.Tag = lang;
+            //    menuLang.IsChecked = lang.Equals(currLang);
+            //    menuLang.Click += ChangeLanguageClick;
+            //    menuLanguage.Items.Add(menuLang);
+            //}
+        }
+
+        private void LanguageChanged(Object sender, EventArgs e)
+        {
+            CultureInfo currLang = App.Language;
+
+            //Отмечаем нужный пункт смены языка как выбранный язык
+            //foreach (MenuItem i in menuLanguage.Items)
+            //{
+            //    CultureInfo ci = i.Tag as CultureInfo;
+            //    i.IsChecked = ci != null && ci.Equals(currLang);
+            //}
+        }
+
+        private void ChangeLanguageClick(Object sender, EventArgs e)
+        {
+            MenuItem mi = sender as MenuItem;
+            if (mi != null)
+            {
+                CultureInfo lang = mi.Tag as CultureInfo;
+                if (lang != null) {
+                    App.Language = lang;
+                }
+            }
+
         }
     }
 }
