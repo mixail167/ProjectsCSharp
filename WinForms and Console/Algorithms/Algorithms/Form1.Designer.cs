6dafe51083b4cc97ae9faafcebcff4db2425d759
mixail167 <mixailkovalev167@mail.ru>
2018-06-12 16:15:15 +0200

    Add AlgKMP

---
+++ WinForms and Console/Algorithms/Algorithms/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Algorithms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(624, 442);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(616, 416);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Алгоритм Кнута-Моррисона-Пратта";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(616, 416);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Исходная строка:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Образ:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(111, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(497, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "abcabeabcabcabd";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(111, 31);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(416, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "abcabd";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(533, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Проверить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 442);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Algorithms";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}
+
---
+++ WinForms and Console/Algorithms/Algorithms/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Algorithms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Алгоритм Кнута-Морриса-Пратта
+        /// </summary>
+        /// <param name="text"> Исходный текст</param>
+        /// <param name="substring">Образ</param>
+        /// <param name="index">Индекс первого вхождения образа в исходном тексте</param>
+        /// <returns>Значение, показывающее, содержится ли образ в тексте</returns>
+        private bool AlgKMP(string text, string substring, out int index)
+        {
+            int[] pi = new int[substring.Length];
+            int j = 0, i = 1;
+            pi[0] = 0;
+            while (i != substring.Length)
+            {
+                if (!substring[i].Equals(substring[j]))
+                {
+                    if (j == 0)
+                    {
+                        pi[i] = 0;
+                        i++;
+                    }
+                    else
+                    {
+                        j = pi[j - 1];
+                    }
+                }
+                else
+                {
+                    pi[i] = j + 1;
+                    i++;
+                    j++;
+                }
+            }
+            i = 0;
+            j = 0;
+            while (i != text.Length)
+            {
+                if (text[i].Equals(substring[j]))
+                {
+                    i++;
+                    j++;
+                    if (j == substring.Length)
+                    {
+                        index = i - substring.Length;
+                        return true;
+                    }
+                }
+                else if (j != 0)
+                {
+                    j = pi[j - 1];
+                }
+                else
+                {
+                    i++;
+                    if (i == text.Length)
+                    {
+                        index = 0;
+                        return false;
+                    }
+                }
+            }
+            index = 0;
+            return false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int k = 0;
+            if (textBox1.Text == string.Empty)
+            {
+                label3.Text = "Ошибка: Исходный текст не задан.";
+            }
+            else if (textBox2.Text == string.Empty)
+            {
+                label3.Text = "Ошибка: Образ не задан.";
+            }
+            else if (textBox1.Text.Length < textBox2.Text.Length)
+            {
+                label3.Text = "Ошибка: Длина образа больше длины исходного текста.";
+            }
+            else if (AlgKMP(textBox1.Text, textBox2.Text, out k))
+            {
+                label3.Text = "Вывод: Образ найден. Индекс первого вхождения: " + k.ToString() + ".";
+            }
+            else
+            {
+                label3.Text = "Вывод: Образ не найден.";
+            }
+        }
+    }
+}
