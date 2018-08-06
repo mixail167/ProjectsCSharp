299a946b8b8b7bb534e9214d84996b280b52eb51
mixail167 <mixailkovalev167@mail.ru>
2018-08-06 10:40:52 +0300

    Удаление ненужных библиотек

--- WinForms and Console/SnakeNew/SnakeNew/Complexity.cs
+++ WinForms and Console/SnakeNew/SnakeNew/Complexity.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Configuration;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 
 namespace SnakeNew
--- WinForms and Console/SnakeNew/SnakeNew/Form1.cs
+++ WinForms and Console/SnakeNew/SnakeNew/Form1.cs
@@ -2,12 +2,8 @@
 using SharpGL.SceneGraph.Assets;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Configuration;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 
 namespace SnakeNew
