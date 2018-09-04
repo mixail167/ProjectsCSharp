a0862faf37ddf915ab582fea714cd4f1b5df90b0
mixail167 <mixailkovalev167@mail.ru>
2018-09-04 16:34:18 +0300

    Добавлен проет для работы с анимацией

---
+++ WinForms and Console/GifAnime/GifAnime/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace GifAnime
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(591, 536);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Создать ленту";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.tableLayoutPanel1.SetRowSpan(this.pictureBox1, 2);
+            this.pictureBox1.Size = new System.Drawing.Size(582, 556);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 75F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 95F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(784, 562);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "GifAnime";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+
+
+    }
+}
+
---
+++ WinForms and Console/GifAnime/GifAnime/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GifAnime
+{
+    public partial class Form1 : Form
+    {
+        bool currentlyAnimating = false;
+        Bitmap animatedImage;
+        private double koef;
+
+        public Form1()
+        {
+            InitializeComponent();
+            animatedImage = Properties.Resources.gif4;
+            double koef2;
+            if (animatedImage.Width >= animatedImage.Height)
+            {
+                koef = pictureBox1.Width * 1.0 / animatedImage.Width;
+            }
+            else
+            {
+                koef = pictureBox1.Height * 1.0 / animatedImage.Height;
+            }
+            koef2 = animatedImage.Height * 1.0 / animatedImage.Width;
+            List<Image> frames = Frames(animatedImage);
+            TableLayoutPanel tableLayoutPanel2 = new TableLayoutPanel()
+            {
+                AutoScroll = true,
+                ColumnCount = 1,
+                RowCount = frames.Count,
+                Dock = DockStyle.Fill
+            };
+            for (int i = 0; i < frames.Count; i++)
+            {
+                PictureBox pictureBox = new PictureBox()
+                {
+                    Parent = tableLayoutPanel2,
+                    Image = frames[i],
+                    Size = new Size(150, (int)(150 * koef2)),
+                    SizeMode = PictureBoxSizeMode.StretchImage
+                };
+                tableLayoutPanel2.Controls.Add(pictureBox, 0, i);
+            }
+            tableLayoutPanel1.Controls.Add(tableLayoutPanel2, 1, 0);
+        }
+
+        public void AnimateImage()
+        {
+            if (!currentlyAnimating)
+            {
+                ImageAnimator.Animate(animatedImage, new EventHandler(this.OnFrameChanged));
+                currentlyAnimating = true;
+            }
+        }
+
+        private void OnFrameChanged(object sender, EventArgs e)
+        {
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            AnimateImage();
+            ImageAnimator.UpdateFrames();
+            e.Graphics.DrawImage(animatedImage, new RectangleF(0, 0, (float)(animatedImage.Width * koef), (float)(animatedImage.Height * koef)));
+        }
+
+        private List<Image> Frames(Image image)
+        {
+            List<Image> frames = new List<Image>();
+            FrameDimension d = new FrameDimension(image.FrameDimensionsList[0]);
+            for (int i = 0; i < image.GetFrameCount(d); i++)
+            {
+                image.SelectActiveFrame(d, i);
+                frames.Add(new Bitmap(image));
+            }
+            return frames;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                
+            }
+        }
+    }
+}
