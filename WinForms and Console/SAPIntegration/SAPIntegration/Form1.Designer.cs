c308f6eeb6e2b909cff375cfa13cfb76ca8f6b97
mixail167 <mixailkovalev167@mail.ru>
2018-09-17 15:43:00 +0300

    Проект интеграции с SAP

---
+++ WinForms and Console/SAPIntegration/SAPIntegration/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace SAPIntegration
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.columnName1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnName2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // columnName1
+            // 
+            this.columnName1.Text = "Name1";
+            this.columnName1.Width = 150;
+            // 
+            // columnName2
+            // 
+            this.columnName2.Text = "Name2";
+            this.columnName2.Width = 150;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnName1,
+            this.columnName2});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(384, 262);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 262);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "SAPIntegration";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ColumnHeader columnName1;
+        private System.Windows.Forms.ColumnHeader columnName2;
+        private System.Windows.Forms.ListView listView1;
+
+    }
+}
+
---
+++ WinForms and Console/SAPIntegration/SAPIntegration/Form1.cs
@@ -0,0 +1,48 @@
+﻿using SAP.Middleware.Connector;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SAPIntegration
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //RfcConfigParameters rfcParams = new RfcConfigParameters();
+            //rfcParams.Add(RfcConfigParameters.AppServerHost, "METdb");
+            //rfcParams.Add(RfcConfigParameters.SystemNumber, "01");
+            //rfcParams.Add(RfcConfigParameters.SystemID, "MET");
+            //rfcParams.Add(RfcConfigParameters.User, "KOVALEV");
+            //rfcParams.Add(RfcConfigParameters.Password, "m19n28j375k");
+            //rfcParams.Add(RfcConfigParameters.Client, "112");
+            //rfcParams.Add(RfcConfigParameters.Language, "RU");
+            //rfcParams.Add(RfcConfigParameters.PoolSize, "10");
+            //RfcDestination sapRfcDestination = RfcDestinationManager.GetDestination(rfcParams);
+            RfcDestination sapRfcDestination = RfcDestinationManager.GetDestination("ProduktiveSystem");
+            RfcRepository sapRfcRepository = sapRfcDestination.Repository;
+            IRfcFunction bapiGetCompanyList = sapRfcRepository.CreateFunction("BAPI_COMPANY_GETLIST");
+            IRfcFunction bapiGetCompanyDetail = sapRfcRepository.CreateFunction("BAPI_COMPANY_GETDETAIL");
+            bapiGetCompanyList.Invoke(sapRfcDestination);
+            IRfcTable companyTable = bapiGetCompanyList.GetTable("COMPANY_LIST");
+            IRfcStructure rfcReturn = bapiGetCompanyList.GetStructure("RETURN");
+            for (int i = 0; i < companyTable.RowCount; i++)
+            {
+                companyTable.CurrentIndex = i;
+                string companyNumber = companyTable.GetString("COMPANY");
+                string name1 = companyTable.GetString("NAME1");
+                bapiGetCompanyDetail.SetValue("COMPANYID", companyNumber);
+                bapiGetCompanyDetail.Invoke(sapRfcDestination);
+                IRfcStructure companyDetail = bapiGetCompanyDetail.GetStructure("COMPANY_DETAIL");
+                string name2 = companyDetail.GetString("NAME2");
+                listView1.Items.Add(new ListViewItem(new string[] { name1, name2 }));
+            }
+        }
+    }
+}
