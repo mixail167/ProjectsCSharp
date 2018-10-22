70521b79038780fcb6736cd3f4ce18e31964aafc
mixail167 <mixailkovalev167@mail.ru>
2018-10-22 12:48:14 +0300

    ReadOnly

--- WinForms and Console/TCP/ChatClient/ChatClient/Form1.Designer.cs
+++ WinForms and Console/TCP/ChatClient/ChatClient/Form1.Designer.cs
@@ -89,6 +89,7 @@
             // 
             this.richTextBox1.Location = new System.Drawing.Point(12, 32);
             this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
             this.richTextBox1.Size = new System.Drawing.Size(450, 250);
             this.richTextBox1.TabIndex = 10;
             this.richTextBox1.Text = "";
