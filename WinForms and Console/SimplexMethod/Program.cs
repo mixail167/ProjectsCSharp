8072da4848fd0f7a1e39c0f810156e8d1d960e0b
mixail167 <mixailkovalev167@mail.ru>
2020-02-17 16:42:14 +0300

    SimplexMethod

--- WinForms and Console/SimplexMethod/Program.cs
+++ WinForms and Console/SimplexMethod/Program.cs
@@ -4,6 +4,25 @@ namespace SimplexMethod
 {
     class Program
     {
+        static void Solve(double[,] tableKoef, double[] limits, double[] targetFunc, bool isMin)
+        {
+            Simplex simplex = new Simplex(tableKoef, limits, targetFunc, isMin);
+            if (simplex.Calculate())
+            {
+                double[] values = simplex.GetValues();
+                double value = simplex.GetTargetFuncValue();
+                for (int i = 0; i < values.Length; i++)
+                {
+                    Console.WriteLine("x[{0}] = {1:f3}", i, values[i]);
+                }
+                Console.WriteLine("F = {0:f3}", value);
+            }
+            else
+            {
+                Console.WriteLine("Задача неразрешима.");
+            }
+        }
+
         static void Main(string[] args)
         {
             double[,] tableKoef = new double[,]
@@ -12,22 +31,61 @@ namespace SimplexMethod
                 { -0.5, 1 }
             };
             double[] limits = new double[] { 3, 1 };
-            double[] centralFunc = new double[] { 1, 2 };
-            Simplex simplex = new Simplex(tableKoef, limits, centralFunc);
-            if (simplex.Calculate())
+            double[] targetFunc = new double[] { 1, 2 };
+            bool isMin = true;
+            Solve(tableKoef, limits, targetFunc, isMin);
+            tableKoef = new double[,]
             {
-                double[] values = simplex.GetValues();
-                double value = simplex.GetCentralFuncValue();
-                for (int i = 0; i < values.Length; i++)
-                {
-                    Console.WriteLine("x[{0}] = {1:f2}", i, values[i]);
-                }
-                Console.WriteLine("F = {0:f2}", value);               
-            }
-            else
+                { 180, 190, 30, 10, 260, 130, 21 },
+                { 20, 3, 40, 865, 310, 3, 2 },
+                { 0, 0, 50, 6, 20, 650, 200 },
+                { 9, 10, 7, 12, 60, 20, 10 }
+            };
+            limits = new double[] { 118, 56, 500, 8 };
+            targetFunc = new double[] { 1.8, 1, 0.28, 3.4, 2.9, 0.5, 0.1 };
+            isMin = true;
+            Solve(tableKoef, limits, targetFunc, isMin);
+            tableKoef = new double[,]
             {
-                Console.WriteLine("Задача неразрешима.");
-            }
+                { 2,12 },
+                { 4,6 },
+                { 3,0 },
+                { 0,18 }
+            };
+            limits = new double[] { 20, 32, 14, 42 };
+            targetFunc = new double[] { 12, 10 };
+            isMin = true;
+            Solve(tableKoef, limits, targetFunc, isMin);
+            tableKoef = new double[,]
+            {
+                { 2,3,6 },
+                { 4,2,4 },
+                { 4,6,8 }
+            };
+            limits = new double[] { 240, 200, 160 };
+            targetFunc = new double[] { 4, 5, 4 };
+            isMin = false;
+            Solve(tableKoef, limits, targetFunc, isMin);
+            tableKoef = new double[,]
+            {
+                { 2,1 },
+                { 1,3 },
+                { 0,1 }
+            };
+            limits = new double[] { 64, 72, 20 };
+            targetFunc = new double[] { 4, 6 };
+            isMin = false;
+            Solve(tableKoef, limits, targetFunc, isMin);
+            tableKoef = new double[,]
+            {
+                { -3,-4 },
+                { 1,3 },
+                { 2,1 }
+            };
+            limits = new double[] { -6, 3, 4 };
+            targetFunc = new double[] { 4, 16 };
+            isMin = false;
+            Solve(tableKoef, limits, targetFunc, isMin);           
             Console.ReadKey();
         }
     }
--- WinForms and Console/SimplexMethod/Simplex.cs
+++ WinForms and Console/SimplexMethod/Simplex.cs
@@ -1,87 +1,83 @@
-﻿namespace SimplexMethod
+﻿using System.Collections.Generic;
+
+namespace SimplexMethod
 {
     class Simplex
     {
         double[,] table;
+        double[] targetFunc;
+        List<int> basis;
+        double[] values;
+        double valueTargetFunc;
+        bool isMin;
 
-        public Simplex(double[,] tableKoef, double[] limits, double[] centralFunc)
+        public Simplex(double[,] tableKoef, double[] limits, double[] targetFunc, bool isMin)
         {
             int n = tableKoef.GetLength(0);
             int m = tableKoef.GetLength(1);
-            table = new double[n + 1, m + 1];
+            this.targetFunc = targetFunc;
+            this.isMin = isMin;
+            table = new double[n, m + n + 1];
+            basis = new List<int>();
+            double k = (isMin) ? -1 : 1;
             for (int i = 0; i < n; i++)
             {
-                table[i + 1, 0] = limits[i];
+                table[i, n + m] = limits[i] * k;
                 for (int j = 0; j < m; j++)
                 {
-                    table[0, j + 1] = centralFunc[j];
-                    table[i + 1, j + 1] = tableKoef[i, j];
+                    table[i, j] = tableKoef[i, j] * k;
+                }
+                if (m + i < m + n)
+                {
+                    table[i, m + i] = 1;
+                    basis.Add(m + i);
                 }
             }
         }
 
         public bool Calculate()
         {
-            while (true)
+            while (!IsPosNegLimits())
             {
-                if (IsOptimalPlan())
-                {
-                    return true;
-                }
-                else if (IsUnsolvability())
-                {
-                    return false;
-                }
-                else
-                {
-                    int column = FindColumn();
-                    int row = FindRow(column);
-                    ConvertTable(row, column);
-                }
+                int row = FindRow();
+                int column = FindColumn(row);
+                ConvertTable(row, column);
             }
-        }
-
-        private void ConvertTable(int row, int column)
-        {
-            int n = table.GetLength(0);
-            int m = table.GetLength(1);
-            double[,] tempTable = new double[n, m];
-            for (int i = 0; i < m; i++)
+            double[] delta = CalculateDelta();
+            while (!CheckDelta(delta))
             {
-                tempTable[row, i] = table[row, i] / table[row, column];
+                int column = FindColumn(delta);
+                int row = FindRow(column);
+                ConvertTable(row, column);
+                delta = CalculateDelta();
             }
-            for (int i = 0; i < n; i++)
-            {
-                if (i != row)
-                {
-                    for (int j = 0; j < m; j++)
-                    {
-                        tempTable[i, j] = table[i, j] - table[i, column] * tempTable[row, j];
-                    }
-                }
-            }
-            table = tempTable;
+            SetValues();
+            return true;
         }
 
         private int FindRow(int column)
         {
             int n = table.GetLength(0);
+            int m = table.GetLength(1);
             int row = 0;
-            for (int i = 1; i < n && row == 0; i++)
+            double q = 0;
+            for (int i = 0; i < n; i++)
             {
                 if (table[i, column] > 0)
                 {
                     row = i;
+                    q = table[i, m - 1] / table[i, column];
+                    break;
                 }
             }
             for (int i = row + 1; i < n; i++)
             {
                 if (table[i, column] > 0)
                 {
-                    double delta1 = table[i, 0] / table[i, column];
-                    double delta2 = table[row, 0] / table[row, column];
-                    if (delta1 < delta2)
+                    double temp = table[i, m - 1] / table[i, column];
+                    if (q >= temp)
                     {
+                        q = temp;
                         row = i;
                     }
                 }
@@ -89,53 +85,135 @@
             return row;
         }
 
-        private int FindColumn()
+        private int FindColumn(double[] delta)
         {
-            int n = table.GetLength(1);
             int column = 0;
-            for (int i = 1; i < n && column == 0; i++)
+            double maxMin = delta[0];
+            for (int i = 1; i < delta.Length - 1; i++)
             {
-                if (table[0, i] > 0)
+                if ((isMin && delta[i] >= maxMin) || (!isMin && delta[i] <= maxMin))
                 {
-                    column = i;
-                }
-            }
-            for (int i = column + 1; i < n; i++)
-            {
-                if (table[0, i] > 0 && table[0, i] >= table[0, column])
-                {
+                    maxMin = delta[i];
                     column = i;
                 }
             }
             return column;
         }
 
-        private bool IsUnsolvability()
+        private bool CheckDelta(double[] delta)
         {
-            int n = table.GetLength(0);
-            int m = table.GetLength(1);
-            for (int i = 1; i < m; i++)
+            for (int i = 0; i < delta.Length - 1; i++)
             {
-                if (table[0, i] > 0)
+                if ((isMin && delta[i] > 0) || (!isMin && delta[i] < 0))
                 {
-                    for (int j = 1; j < n; j++)
-                    {
-                        if (table[j, i] > 0)
-                        {
-                            return false;
-                        }
-                    }
+                    return false;
                 }
             }
             return true;
         }
 
-        private bool IsOptimalPlan()
+        private double[] CalculateDelta()
         {
-            int n = table.GetLength(1);
+            int n = table.GetLength(0);
+            int m = table.GetLength(1);
+            double[] delta = new double[m];
+            for (int j = 0; j < m; j++)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    if (basis[i] < targetFunc.Length)
+                    {
+                        delta[j] += targetFunc[basis[i]] * table[i, j];
+                    }
+                }
+                if (j < targetFunc.Length)
+                {
+                    delta[j] -= targetFunc[j];
+                }
+            }
+            return delta;
+        }
+
+
+        private void SetValues()
+        {
+            values = new double[targetFunc.Length];
+            int m = table.GetLength(1);
+            for (int i = 0; i < values.Length; i++)
+            {
+                int j = basis.IndexOf(i);
+                if (j != -1)
+                {
+                    values[i] = table[j, m - 1];
+                    valueTargetFunc += values[i] * targetFunc[i];
+                }
+            }
+        }
+
+        private void ConvertTable(int row, int column)
+        {
+            basis[row] = column;
+            int n = table.GetLength(0);
+            int m = table.GetLength(1);
+            double[,] tempTable = new double[n, m];
+            for (int j = 0; j < m; j++)
+            {
+                tempTable[row, j] = table[row, j] / table[row, column];
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if (i == row)
+                {
+                    continue;
+                }
+                for (int j = 0; j < m; j++)
+                {
+                    tempTable[i, j] = table[i, j] - table[i, column] * tempTable[row, j];
+                }
+            }
+            table = tempTable;
+        }
+
+        private int FindRow()
+        {
+            int n = table.GetLength(0);
+            int m = table.GetLength(1);
+            int row = 0;
+            double min = table[0, m - 1];
             for (int i = 1; i < n; i++)
             {
-                if (table[0, i] > 0)
+                if (table[i, m - 1] <= min)
+                {
+                    min = table[i, m - 1];
+                    row = i;
+                }
+            }
+            return row;
+        }
+
+        private int FindColumn(int row)
+        {
+            int m = table.GetLength(1);
+            int column = 0;
+            double min = table[row, 0];
+            for (int i = 1; i < m - 1; i++)
+            {
+                if (table[row, i] <= min)
+                {
+                    min = table[row, i];
+                    column = i;
+                }
+            }
+            return column;
+        }
+
+        private bool IsPosNegLimits()
+        {
+            int n = table.GetLength(0);
+            int m = table.GetLength(1);
+            for (int i = 0; i < n; i++)
+            {
+                if (table[i, m - 1] < 0)
                 {
                     return false;
                 }
@@ -145,18 +223,12 @@
 
         public double[] GetValues()
         {
-            int n = table.GetLength(0);
-            double[] values = new double[n - 1];
-            for (int i = 1; i < n; i++)
-            {
-                values[i - 1] = table[i, 0];
-            }
             return values;
         }
 
-        public double GetCentralFuncValue()
+        public double GetTargetFuncValue()
         {
-            return -table[0, 0];
+            return valueTargetFunc;
         }
     }
 }
