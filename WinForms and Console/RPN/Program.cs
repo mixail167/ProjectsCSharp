4a2afcf5133f6b3556b8c757017785fbd98be702
mixail167 <mixailkovalev167@mail.ru>
2020-10-23 13:39:33 +0300

    ОПЗ

---
+++ WinForms and Console/RPN/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RPN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Выражение: ");
+            string input = Console.ReadLine();
+            RPN rpn = new RPN(input);
+            string output = string.Join(string.Empty, rpn.ConvertToPostfixNotation());
+            decimal result = rpn.Result();
+            Console.WriteLine("ОПЗ: " + output);
+            Console.WriteLine("Результат: " + result);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WinForms and Console/RPN/RPN.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RPN
+{
+    public class RPN
+    {
+        readonly string input;
+
+        public RPN(string input)
+        {
+            operators = new List<string>(standart_operators);
+            this.input = input;
+        }
+
+        private readonly List<string> operators;
+        private readonly List<string> standart_operators = new List<string>(new string[] { "(", ")", "+", "-", "*", "/", "^" });
+
+        private IEnumerable<string> Separate()
+        {
+            int pos = 0;
+            while (pos < input.Length)
+            {
+                string s = string.Empty + input[pos];
+                if (!standart_operators.Contains(input[pos].ToString()))
+                {
+                    if (char.IsDigit(input[pos]))
+                    {
+                        for (int i = pos + 1; i < input.Length && (char.IsDigit(input[i]) || input[i] == ',' || input[i] == '.'); i++)
+                        {
+                            s += input[i];
+                        }
+                    }
+                    else if (char.IsLetter(input[pos]))
+                    {
+                        for (int i = pos + 1; i < input.Length && (char.IsLetter(input[i]) || char.IsDigit(input[i])); i++)
+                        {
+                            s += input[i];
+                        }
+                    }
+                }
+                yield return s;
+                pos += s.Length;
+            }
+        }
+
+        private byte GetPriority(string s)
+        {
+            switch (s)
+            {
+                case "(":
+                case ")":
+                    return 0;
+                case "+":
+                case "-":
+                    return 1;
+                case "*":
+                case "/":
+                    return 2;
+                case "^":
+                    return 3;
+                default:
+                    return 4;
+            }
+        }
+
+        public string[] ConvertToPostfixNotation()
+        {
+            List<string> outputSeparated = new List<string>();
+            Stack<string> stack = new Stack<string>();
+            foreach (string c in Separate())
+            {
+                if (operators.Contains(c))
+                {
+                    if (stack.Count > 0 && !c.Equals("("))
+                    {
+                        if (c.Equals(")"))
+                        {
+                            string s = stack.Pop();
+                            while (s != "(")
+                            {
+                                outputSeparated.Add(s);
+                                s = stack.Pop();
+                            }
+                        }
+                        else if (GetPriority(c) > GetPriority(stack.Peek()))
+                        {
+                            stack.Push(c);
+                        }
+                        else
+                        {
+                            while (stack.Count > 0 && GetPriority(c) <= GetPriority(stack.Peek()))
+                            {
+                                outputSeparated.Add(stack.Pop());
+                            }
+                            stack.Push(c);
+                        }
+                    }
+                    else
+                    {
+                        stack.Push(c);
+                    }
+                }
+                else
+                {
+                    outputSeparated.Add(c);
+                }
+            }
+            if (stack.Count > 0)
+            {
+                foreach (string c in stack)
+                {
+                    outputSeparated.Add(c);
+                }
+            }
+            return outputSeparated.ToArray();
+        }
+
+        public decimal Result()
+        {
+            Stack<string> stack = new Stack<string>();
+            Queue<string> queue = new Queue<string>(ConvertToPostfixNotation());
+            string str = queue.Dequeue();
+            while (queue.Count >= 0)
+            {
+                if (!operators.Contains(str))
+                {
+                    stack.Push(str);
+                    str = queue.Dequeue();
+                }
+                else
+                {
+                    decimal summ = 0;
+                    try
+                    {
+
+                        switch (str)
+                        {
+
+                            case "+":
+                                {
+                                    decimal a = Convert.ToDecimal(stack.Pop());
+                                    decimal b = Convert.ToDecimal(stack.Pop());
+                                    summ = a + b;
+                                    break;
+                                }
+                            case "-":
+                                {
+                                    decimal a = Convert.ToDecimal(stack.Pop());
+                                    decimal b = Convert.ToDecimal(stack.Pop());
+                                    summ = b - a;
+                                    break;
+                                }
+                            case "*":
+                                {
+                                    decimal a = Convert.ToDecimal(stack.Pop());
+                                    decimal b = Convert.ToDecimal(stack.Pop());
+                                    summ = b * a;
+                                    break;
+                                }
+                            case "/":
+                                {
+                                    decimal a = Convert.ToDecimal(stack.Pop());
+                                    decimal b = Convert.ToDecimal(stack.Pop());
+                                    summ = b / a;
+                                    break;
+                                }
+                            case "^":
+                                {
+                                    decimal a = Convert.ToDecimal(stack.Pop());
+                                    decimal b = Convert.ToDecimal(stack.Pop());
+                                    summ = Convert.ToDecimal(Math.Pow(Convert.ToDouble(b), Convert.ToDouble(a)));
+                                    break;
+                                }
+                        }
+                    }
+                    catch { }
+                    stack.Push(summ.ToString());
+                    if (queue.Count > 0)
+                    {
+                        str = queue.Dequeue();
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            return Convert.ToDecimal(stack.Pop());
+        }
+    }
+}
